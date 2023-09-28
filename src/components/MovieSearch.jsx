8fbe8a66437008baeb6154a475adc9d2322e5e47
Sean R Reid <sean@torchcodelab.com>
2023-09-28 16:16:19 -0400

    Added Movie Search form

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -3,23 +3,47 @@ import { CardWrapper } from './UI';
 import { MovieCard } from './MovieCard';
 
 export const MovieSearch = () => {
-    const [movie, setMovie] = useState(null);
+    const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
-    const getMovie = async () => {
-        console.log(movie);
-        const apiUrl = 'http://www.omdbapi.com/?t=Hackers&apikey=66f230e4';
+    const getMovie = async (movieTitle) => {
+        const apiUrl = `http://www.omdbapi.com/?t=${movieTitle}&apikey=66f230e4`;
         const data = await fetch(apiUrl).then((response) => response.json());
-        setMovie(data);
+        setMovies([...movies, data]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getMovie(searchValue);
+        setSearchValue('');
+    };
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
     };
 
     return (
         <>
             <section>
-                <button onClick={getMovie}>Get Movie</button>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Search For a Move by Title
+                        <input
+                            type='text'
+                            value={searchValue}
+                            placeholder='What title should I look up?'
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <button type='submit'>Get Movie</button>
+                </form>
             </section>
             <section>
                 <CardWrapper>
-                    {!!movie && <MovieCard movie={movie} />}
+                    {movies.length > 0 &&
+                        movies.map((movie) => (
+                            <MovieCard key={movie.imdbID} movie={movie} />
+                        ))}
                 </CardWrapper>
             </section>
         </>
