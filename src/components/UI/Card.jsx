6b78f2b3f7fb334519b038f195d2091af97d625d
Sean R Reid <sean@torchcodelab.com>
2023-09-29 09:34:05 -0400

    Cleaned up Card styles

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -11,6 +11,7 @@ export const Card = styled.div`
     background-color: #eaeaea;
     border-radius: 4px;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    color: #111010;
     display: flex;
     flex-wrap: wrap;
     margin: 0.5rem;
@@ -19,16 +20,18 @@ export const Card = styled.div`
     width: 100%;
 
     img {
+        aspect-ratio: 2/3;
         height: auto;
         width: 100%;
     }
 
-    .caption {
-        color: #111010;
-        padding: 8px 16px;
+    .details {
+        padding: 8px 16px 16px;
+        width: 100%;
     }
 
-    .caption h2 {
+    h2,
+    h3 {
         margin: 0.5rem 0;
     }
 `;
